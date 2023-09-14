164778982572443540dd44ecde93e20414ddc560
hunte <hunte@DESKTOP-PJI2CC2>
2023-09-14 08:58:03 -0400

    Added more comments

--- binary_search/binary_search_.cpp
+++ binary_search/binary_search_.cpp
@@ -47,6 +47,9 @@ int main() {
     int target5 = 7;
     cout << "Target 7 found at index: " << binarySearch(vec1, target5) << endl; // Expected output: 5
 
+    // Test case 6: Target exosts at the front of the vector
+    int target6 = 1;
+    cout << "Target 1 found at index: " << binarySearch(vec1, target6) << endl; // Expected output: 0
     return 0;
 }
 
@@ -54,26 +57,26 @@ int main() {
 int binarySearchHelp(const vector<int>& inputVector, const int& target, int front, int end) {
     int middleIndex = (front + end) / 2;
     
-    if (target == inputVector.at(middleIndex)) return middleIndex;
+    if (target == inputVector.at(middleIndex)) return middleIndex; //Target number was found
 
     if (target < inputVector.at(middleIndex)) {
 
-        if (middleIndex == 0) return -1;
+        if (middleIndex == 0) return -1; // Target was not located at the front                               
 
-        if (front == end) return -1;
+        if (front == end) return -1;     // Target was not located at the end
 
-        return binarySearchHelp(inputVector, target, front, middleIndex);
+        return binarySearchHelp(inputVector, target, front, middleIndex); // Target value was less than t
     }
 
     if (target > inputVector.at(middleIndex)) {
 
         int lastIndex = inputVector.size() - 1;
         
-        if (front + 1 == lastIndex) return binarySearchHelp(inputVector, target, end, end);
+        if (front + 1 == lastIndex) return binarySearchHelp(inputVector, target, end, end); // Checks the end of the vector
         
-        if ((front == lastIndex) && (end == lastIndex)) return -1;
+        if ((front == lastIndex) && (end == lastIndex)) return -1; // Target was larger that all the numbers in the vector
 
-        return binarySearchHelp(inputVector, target, middleIndex, end);
+        return binarySearchHelp(inputVector, target, middleIndex, end); // Target was larger than the middle index
     }
 }
 
