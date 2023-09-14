e7c188925d3ee26287d032baf823ee5fbe3a168d
hunte <hunte@DESKTOP-PJI2CC2>
2023-09-13 20:14:45 -0400

    Add project files.

---
+++ binary_search/binary_search_.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binarySearch(const vector<int>& inputVector, const int& target, int front, int end);
+
+int main() {
+
+	return 0;
+}
+
+int binarySearch(const vector<int>& inputVector, const int& target, int front, int end) {
+	if (inputVector.at(end / 2) == target) return (end / 2);
+
+	if (inputVector.at( (end - front) / 2 ) > target) {
+		front = end / 2;
+		return binarySearch(inputVector, target, front, end);
+	}
+
+	if (inputVector.at( (end - front) / 2 ) < target) {
+		end /= 2;
+		return binarySearch(inputVector, target, front, end);
+	}
+
+
+}
