24e25664855080884befe82c8e20dcd50ba8c112
hunte <hunte@DESKTOP-PJI2CC2>
2023-09-14 07:53:25 -0400

    Accounted for edge cases when the index is at the rightmost or leftmost positions.

--- binary_search/binary_search_.cpp
+++ binary_search/binary_search_.cpp
@@ -3,24 +3,41 @@
 
 using namespace std;
 
-int binarySearch(const vector<int>& inputVector, const int& target, int front, int end);
+int binarySearchHelp(const vector<int>& inputVector, const int& target, int front, int end);
+int binarySearch(const vector<int>& inputVector, const int& target);
 
 int main() {
+	vector<int> vec1 = { 1, 2, 3, 4, 5, 7};
+	cout << binarySearch(vec1, 8); 
 	return 0;
 }
 
-int binarySearch(const vector<int>& inputVector, const int& target, int front, int end) {
-	if (inputVector.at(end / 2) == target) return (end / 2);
+int binarySearchHelp(const vector<int>& inputVector, const int& target, int front, int end) {
 
-	if (inputVector.at( (end - front) / 2 ) > target) {
-		front = end / 2;
-		return binarySearch(inputVector, target, front, end);
+	int middleIndex = (front + end) / 2;
+
+	if (target == inputVector.at(middleIndex)) return middleIndex;
+
+
+	if (target < inputVector.at(middleIndex)) {
+		if (middleIndex == 0) return -1;
+
+		return binarySearchHelp(inputVector, target, front, middleIndex);
 	}
 
-	if (inputVector.at( (end - front) / 2 ) < target) {
-		end /= 2;
-		return binarySearch(inputVector, target, front, end);
+	if (target > inputVector.at(middleIndex)) {
+		int lastIndex = inputVector.size() - 1;
+
+		if (front + 1 == end) return binarySearchHelp(inputVector, target, end, end);
+
+		if ((front == lastIndex) && (end == lastIndex)) return -1; //Reaches this statement iff target is not found
+
+		return binarySearchHelp(inputVector, target, middleIndex, end);
 	}
+}
 
-
+int binarySearch(const vector<int>& inputVector, const int& target) {
+	int FRONT = 0;
+	int END = inputVector.size() - 1;
+	return binarySearchHelp(inputVector, target, FRONT, END);
 }
