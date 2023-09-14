1b54b76fa752732a0267c827ab921d223ca826c0
hunte <hunte@DESKTOP-PJI2CC2>
2023-09-14 08:45:12 -0400

    Fixed bug where the target was less than the end index but greater than the rest of the entries

--- binary_search/binary_search_.cpp
+++ binary_search/binary_search_.cpp
@@ -1,3 +1,18 @@
+// Name: Hunter Burnett
+// Date: 9/13/23
+// Program: ECGR 3180 - Homework 3: Recursive Binary Search 
+// Description:
+// 
+// Write a recursive function that takes an input a sorted vector of integers, and a number to be searched.
+// It returns the index of the number if it exists, and -1 if it does not.
+// Important: Your implementation needs to be recursive.
+// 
+// -----Be sure to take of edge cases-----
+// 1. The element might not exist in the array.
+// 2. The target value could be in the rightmost or left most side of the array
+// 3. The target value could be the initial, middle element
+// 4. Duplicate entries could exist in the array
+
 #include <iostream>
 #include <vector>
 
@@ -10,11 +25,27 @@ int binarySearchHelp(const vector<int>& inputVector, const int& target, int fron
 int binarySearch(const vector<int>& inputVector, const int& target);
 
 int main() {
-    // Define a sorted vector
     vector<int> vec1 = { 1, 2, 3, 4, 5, 7 };
 
-    // Call binarySearch to find the index of the target value 8
-    cout << binarySearch(vec1, 8);
+    // Test case 1: Target exists in the vector
+    int target1 = 3;
+    cout << "Target 3 found at index: " << binarySearch(vec1, target1) << endl; // Expected output: 2
+
+    // Test case 2: Target is smaller than the smallest element in the vector
+    int target2 = 0;
+    cout << "Target 0 not found: " << binarySearch(vec1, target2) << endl; // Expected output: -1
+
+    // Test case 3: Target is larger than the largest element in the vector
+    int target3 = 8;
+    cout << "Target 8 not found: " << binarySearch(vec1, target3) << endl; // Expected output: -1
+
+    // Test case 4: Target is larger than the smallest element but smaller than the largest element
+    int target4 = 6;
+    cout << "Target 6 not found: " << binarySearch(vec1, target4) << endl; // Expected output: -1
+
+    // Test case 5: Target exists at the end of the vector
+    int target5 = 7;
+    cout << "Target 7 found at index: " << binarySearch(vec1, target5) << endl; // Expected output: 5
 
     return 0;
 }
@@ -22,39 +53,29 @@ int main() {
 // Recursive function to perform binary search
 int binarySearchHelp(const vector<int>& inputVector, const int& target, int front, int end) {
     int middleIndex = (front + end) / 2;
+    
+    if (target == inputVector.at(middleIndex)) return middleIndex;
 
-    // If the target is found at the middle index, return the index
-    if (target == inputVector.at(middleIndex))
-        return middleIndex;
-
-    // If the target is smaller than the middle element, search in the left half
     if (target < inputVector.at(middleIndex)) {
-        // If the middle index is 0, it means the target is not in the vector
-        if (middleIndex == 0)
-            return -1;
 
-        // Recursively search in the left half
+        if (middleIndex == 0) return -1;
+
+        if (front == end) return -1;
+
         return binarySearchHelp(inputVector, target, front, middleIndex);
     }
 
-    // If the target is larger than the middle element, search in the right half
     if (target > inputVector.at(middleIndex)) {
         int lastIndex = inputVector.size() - 1;
+        
+        if (front + 1 == lastIndex) return binarySearchHelp(inputVector, target, end, end);
+        
+        if ((front == lastIndex) && (end == lastIndex)) return -1;
 
-        // If the search range has been reduced to just two elements and the target is not found, return -1
-        if (front + 1 == end)
-            return -1;
-
-        // If the search range is at the very end of the vector and the target is not found, return -1
-        if ((front == lastIndex) && (end == lastIndex))
-            return -1;
-
-        // Recursively search in the right half
         return binarySearchHelp(inputVector, target, middleIndex, end);
     }
 }
 
-// Wrapper function to start binary search
 int binarySearch(const vector<int>& inputVector, const int& target) {
     int FRONT = 0;
     int END = inputVector.size() - 1;
